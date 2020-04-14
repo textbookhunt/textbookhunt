7fecc3839ac6b0d754e3524e1a64f265d6f826a7
jimanaka <jimanaka@hawaii.edu>
2020-04-14 13:48:21 -1000

    update profile routing

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -34,7 +34,7 @@ class NavBar extends React.Component {
           ) : (
             <Dropdown text={this.props.currentUser} pointing="top right" icon={'user'}>
               <Dropdown.Menu>
-                <Dropdown.Item icon="address card" text="Profile" as={NavLink} exact to="/signup"/>
+                <Dropdown.Item icon="address card" text="Profile" as={NavLink} exact to="/profile"/>
                 <Dropdown.Item icon="sign out" text="Sign Out" as={NavLink} exact to="/signout"/>
               </Dropdown.Menu>
             </Dropdown>
