da4ff107bec26490ec9bad9ba0ddf0f5e226c502
Shun <47809066+saitoshi@users.noreply.github.com>
2020-04-14 09:44:37 -1000

    April 14 Update Edit

--- app/imports/ui/pages/AddBook.jsx
+++ app/imports/ui/pages/AddBook.jsx
@@ -5,7 +5,7 @@ import swal from 'sweetalert';
 import { Meteor } from 'meteor/meteor';
 import 'uniforms-bridge-simple-schema-2'; // required for Uniforms
 import SimpleSchema from 'simpl-schema';
-import { Books} from '../../api/book/Book';
+import { Books } from '../../api/book/Book';
 
 /** Create a schema to specify the structure of the data to appear in the form. */
 const formSchema = new SimpleSchema({
