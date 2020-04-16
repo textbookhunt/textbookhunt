160b10aed1a49a6855927ec89daceaf0ca4539c9
jimanaka <jimanaka@hawaii.edu>
2020-04-15 14:36:37 -1000

    landing mockup

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Divider, Header } from 'semantic-ui-react';
+import { Divider, Header, Grid, Container } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
@@ -14,7 +14,14 @@ class Landing extends React.Component {
                 Welcome to Textbook Hunt!
               </Header>
             </Divider>
-            <div className='landing-text'>DESCRPTION prijpifgjspifghsdfjgdjfhn</div>
+            <Container>
+            <div className='landing-text'>
+              Testbook Hunt is a platform that helps provide students with an easy way to buy and sell textbooks from
+              other students.<br/><br/>
+              If you are a first time user, feel free to refer to the user guide below. If not, simply sign in to your
+              account and enjoy!<br/>
+            </div>
+            </Container>
           </div>
 
           <div className='yellow-landing-item'>
@@ -23,7 +30,20 @@ class Landing extends React.Component {
                 Login or Create an Account
               </Header>
             </Divider>
-            <div className='landing-text'>DESCRPTION prijpifgjspifghsdfjgdjfhn</div>
+            <Grid container relaxed divided columns='equal'>
+              <Grid.Column>
+                <div className='landing-text'>
+                  To Sign in, click the user icon on the top right of the website. After that, enter your email and
+                  password.
+                </div>
+              </Grid.Column>
+              <Grid.Column>
+                <div className='landing-text'>
+                  To Sign up, click the user icon on the tip right of the website. Enter your email, password, and any
+                  other needed information. Click submite and you're done!
+                </div>
+              </Grid.Column>
+            </Grid>
           </div>
 
           <div className='black-landing-item'>
@@ -32,7 +52,12 @@ class Landing extends React.Component {
                 Browse Textbooks on Sale
               </Header>
             </Divider>
-            <div className='landing-text'>DESCRPTION prijpifgjspifghsdfjgdjfhn</div>
+            <Container>
+            <div className='landing-text'>
+              To browse textbook listings, first sign in. Then click the "view listings" tab on the nav bar. From here
+              you can see and search for any listing you want.
+            </div>
+            </Container>
           </div>
 
           <div className='yellow-landing-item'>
@@ -41,7 +66,14 @@ class Landing extends React.Component {
                 Sell Your Textbooks
               </Header>
             </Divider>
-            <div className='landing-text'>DESCRPTION prijpifgjspifghsdfjgdjfhn</div>
+            <Container>
+              <div className='landing-text'>
+                To start selling textbooks, first sign in. Then, click on the "post listing" tab on the navigation bar.
+                Fill out the required information and click submit. Your textbook should not be visible to anyone
+                looking
+                to buy.
+              </div>
+            </Container>
           </div>
         </div>
     );
