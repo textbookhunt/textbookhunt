4397292f598a353bb9d5cb8c6cbbdefc47ca22ee
jimanaka <jimanaka@hawaii.edu>
2020-04-27 17:14:20 -1000

    update edit profile

--- app/imports/ui/pages/EditProfile.jsx
+++ app/imports/ui/pages/EditProfile.jsx
@@ -13,23 +13,15 @@ import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import 'uniforms-bridge-simple-schema-2';
-import SimpleSchema from 'simpl-schema';
-import { Tracker } from 'meteor/tracker';
 import { UserInfo, UserInfoSchema } from '../../api/userinfo/UserInfo';
 
-const FormSchema = new SimpleSchema({
-  firstName: String,
-  lastName: String,
-  major: String,
-  description: String,
-}, { tracker: Tracker });
 
 /** Renders the Page for editing a single document. */
 class EditProfile extends React.Component {
   /** On successful submit, insert the data. */
   submit(data) {
-    const { firstName, lastName, major, description, _id } = data;
-    UserInfo.update(_id, { $set: { firstName, lastName, major, description } }, (error) => (error ?
+    const { user, firstName, lastName, major, description, _id } = data;
+    UserInfo.update(_id, { $set: { user, firstName, lastName, major, description } }, (error) => (error ?
       swal('Error', error.message, 'error') :
       swal('Success', 'Item updated successfully', 'success')));
   }
@@ -47,11 +39,11 @@ class EditProfile extends React.Component {
             <Header as="h2" textAlign="center">Edit Profile</Header>
             <AutoForm schema={UserInfoSchema} onSubmit={data => this.submit(data)} model={this.props.doc}>
               <Segment>
+                <TextField class='disabled field' name='user'/>
                 <TextField name = 'firstName'/>
                 <TextField name = 'lastName'/>
                 <TextField name = 'major'/>
                 <LongTextField name = 'description'/>
-                <TextField name='user'/>
                 <SubmitField value='Submit'/>
                 <ErrorsField/>
               </Segment>
@@ -72,11 +64,11 @@ EditProfile.propTypes = {
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(({ match }) => {
   // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
-  const documentId = match.params._id;
-  // Get access to Profile documents.
+  const userAccount = Meteor.users.findOne(match.params._id);
+  const userName = userAccount ? userAccount.username : '';
   const subscription = Meteor.subscribe('UserInfo');
   return {
-    doc: UserInfo.findOne(documentId) ? UserInfo.findOne(documentId) : {},
+    doc: UserInfo.findOne({ user: userName }) ? UserInfo.findOne({ user: userName }) : {},
     ready: subscription.ready(),
   };
 })(EditProfile);
