d405f843b13739ec176fe476ba765b4a98076bb6
jimanaka <jimanaka@hawaii.edu>
2020-05-13 13:50:18 -1000

    default profiles

--- app/imports/startup/server/Accounts.js
+++ app/imports/startup/server/Accounts.js
@@ -1,11 +1,13 @@
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 import { Roles } from 'meteor/alanning:roles';
+import { UserInfo } from '../../api/userinfo/UserInfo';
 
 /* eslint-disable no-console */
 
-function createUser(email, password, role) {
+function createUser(email, password, role, firstName, lastName, major, image, description) {
   console.log(`  Creating user ${email}.`);
+  const user = email;
   const userID = Accounts.createUser({
     username: email,
     email: email,
@@ -14,13 +16,19 @@ function createUser(email, password, role) {
   if (role === 'admin') {
     Roles.addUsersToRoles(userID, 'admin');
   }
+  UserInfo.insert({ user, firstName, lastName, image, major, description });
 }
 
 /** When running app for first time, pass a settings file to set up a default user account. */
 if (Meteor.users.find().count() === 0) {
   if (Meteor.settings.defaultAccounts) {
     console.log('Creating the default user(s)');
-    Meteor.settings.defaultAccounts.map(({ email, password, role }) => createUser(email, password, role));
+    Meteor.settings.defaultAccounts.map(({
+                                           email, password, role, firstName,
+                                           lastName, major, image, description,
+                                         }) => createUser(
+        email, password, role, firstName, lastName, major, image, description,
+    ));
   } else {
     console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
   }
